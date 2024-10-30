62c6e40196245b9944d9467677b32e2c64960632
morixxfoxdata <morixx.datascience@gmail.com>
2024-10-30 14:55:21 +0900

    first

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from utils.speckle_generate import generate_mask_pattern
 
 
 def main():
-    gen1 = generate_mask_pattern()
+    gen1 = generate_mask_pattern(time_length=10, num_x_pixel=200, num_y_pixel=200)
     print(gen1.shape)
 
 
